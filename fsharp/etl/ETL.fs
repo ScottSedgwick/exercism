f9e8e12cc373cded0cf867cef5b0472a6db585a0
Scott Sedgwick <ssedgwick@cochlear.com>
2015-02-27 10:30:19 +1100

    F# ETL
    Committed solution for F# ETL.
    Visual Studio decided to create a .gitattributes file.  I'll keep it.

---
+++ fsharp/etl/ETL.fs
@@ -0,0 +1,8 @@
+﻿namespace ETL
+
+type ETL() = 
+    member this.transform(old) = 
+        [for KeyValue(k, vs) in old do
+           for v in vs do 
+             yield v.ToString().ToLower(), k]
+        |> Map.ofSeq
