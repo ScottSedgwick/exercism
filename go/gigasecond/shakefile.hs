32a7b12d567238c0ee5de3b83c0d558a81fae422
Scott Sedgwick <ssedgwick@cochlear.com>
2015-03-05 16:51:53 +1100

    Shakefiles for go
    Created Shakefiles for all the Go projects.

---
+++ go/gigasecond/shakefile.hs
@@ -0,0 +1,22 @@
+import Development.Shake
+import Development.Shake.FilePath
+import System.Info
+
+srcFile = "gigasecond.go"
+
+main :: IO()
+main = shakeArgs shakeOptions $ do
+	want ["test"]
+
+	"fmt" ~> cmd "go" "fmt"
+
+	"test" ~> do
+		need ["fmt"]
+		cmd "go" "test"
+
+	"submit" ~> do
+		need ["test"]
+		cmd "exercism" "submit" srcFile
+
+	"fetch" ~> 
+		cmd "exercism" "fetch" "go"
