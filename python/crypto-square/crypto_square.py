ae5836921894e679625732f86759f68fc97aec6b
Scott Sedgwick <ssedgwick@cochlear.com>
2015-04-29 15:34:46 +1000

    Committed solution for Python Crypto Square problem.

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,18 +1,17 @@
 import math
 import re
+from itertools import zip_longest
 
 def encode(plaintext):
   if (plaintext == ''):
     return ''
-  pt = re.sub(r'[^a-z0-9]', "", plaintext.lower())
-  n = math.ceil(math.sqrt(len(pt)))
-  at = chunk(pt, n)
-  print(at)
-  tt = list(zip(*at))
-  print(tt)
-  st = [''.join(ss) for ss in tt]
-  print(st)
-  return ' '.join(st)
+  plaintext = re.sub(r'[^a-z0-9]', "", plaintext.lower())
+  size = math.ceil(math.sqrt(len(plaintext)))
+  return ' '.join([''.join(s) for s in transpose(chunk(plaintext, size))])
+
+def transpose(a):
+  max_len = max(len(i) for i in a)
+  return [[i[o] if len(i) > o else '' for i in a] for o in range(max_len)]
 
 def chunk(text, size):
   return [text[i:i+size] for i in range(0, len(text), size)]
