63516436ae773022d50870fc0ceefecd07716f89
Scott Sedgwick <ssedgwick@cochlear.com>
2015-06-23 16:32:48 +1000

    Made changes to C# stbash cipher.

--- csharp/atbash-cipher/atbash-cipher/atbash-cipher.cs
+++ csharp/atbash-cipher/atbash-cipher/atbash-cipher.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
+using System.Runtime.InteropServices;
 
 public class Atbash
 {
@@ -11,30 +12,21 @@ public class Atbash
         return String.Join(" ", SplitByLength(EncodeString(words), 5));
     }
 
-    private static readonly IEnumerable<int> Numbers = Enumerable.Range('0', 10);
-    private static readonly IEnumerable<int> Letters = Enumerable.Range('a', 26);
-    private static readonly IEnumerable<int> ValidChars = Numbers.Concat(Letters);
-
     private static char EncodeChar(char c)
     {
-        return (Numbers.Contains(c)) ? c : (char)('z' - c + 'a');
+        return (Char.IsDigit(c)) ? c : (char)('z' - c + 'a');
     }
 
     private static string EncodeString(string s)
     {
-        return new string(Filter(s).Select(EncodeChar).ToArray());
+        return new string(s.ToLower().Where(Char.IsLetterOrDigit).Select(EncodeChar).ToArray());
     }
 
-    private static string Filter(string words)
+    private static IEnumerable<string> SplitByLength(IEnumerable<char> s, int len)
     {
-        return new string(words.ToLower().Where(c => ValidChars.Contains(c)).ToArray());
-    }
-
-    private static IEnumerable<string> SplitByLength(string str, int maxLength)
-    {
-        for (var index = 0; index < str.Length; index += maxLength)
-        {
-            yield return str.Substring(index, Math.Min(maxLength, str.Length - index));
-        }
-    }
+        return s
+            .Select((x, i) => new {Index = i, Value = x})
+            .GroupBy(x => x.Index / len)
+            .Select(x => new string(x.Select(v => v.Value).ToArray()));
+    } 
 }
