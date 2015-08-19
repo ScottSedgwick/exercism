6ddf147a2b5f0d58e3e74b3df22618c44bcdcba1
Scott Sedgwick <ssedgwick@cochlear.com>
2015-08-19 14:22:37 +1000

    Fixed shakefile. Modified whitespace in minesweeper.hs

--- haskell/minesweeper/shakefile.hs
+++ haskell/minesweeper/shakefile.hs
@@ -1,6 +1,7 @@
 import Development.Shake
 import Development.Shake.FilePath
 import System.Info
+import Prelude hiding ((*>))
 
 problem = "minesweeper"
 
@@ -16,14 +17,14 @@ tmpFile = srcFile ++ ".tmp"
 
 hindent :: String -> Action(String)
 hindent s = do
-	Stdout out <- command [Stdin s] "hindent" ["--style", "gibiansky"]
-	return out
+    Stdout out <- command [Stdin s] "hindent" ["--style", "gibiansky"]
+    return out
 
 diffHIndent :: FilePath -> Action()
 diffHIndent f = do
-	s <- readFile' f
-	tmp <- hindent s
-	checkDiff f s tmp
+    s <- readFile' f
+    tmp <- hindent s
+    checkDiff f s tmp
 
 checkDiff :: FilePath -> String -> String -> Action()
 checkDiff f s tmp | s == tmp  = putQuiet "No HIndent changes required"
@@ -39,38 +40,38 @@ diff f tmp = do
 
 main :: IO()
 main = shakeArgs shakeOptions $ do
-	want ["test"]
- 	
- 	"clean" ~> 
- 		removeFilesAfter "." ["*.o", "*.hi", exeFile]
+    want ["test"]
+     
+    "clean" ~> 
+         removeFilesAfter "." ["*.o", "*.hi", exeFile]
 
-	exeFile *> \_ -> do
-		need [srcFile, tstFile]
-		cmd "ghc" "--make" "-Wall" tstFile
+    exeFile *> \_ -> do
+        need [srcFile, tstFile]
+        cmd "ghc" "--make" "-Wall" tstFile
 
-	"lint" ~> do
-		need [srcFile]
-		cmd "hlint" srcFile
+    "lint" ~> do
+        need [srcFile]
+        cmd "hlint" srcFile
 
-	"diff" ~> do
-		diffHIndent srcFile
+    "diff" ~> do
+        diffHIndent srcFile
 
-	"checkIndent" ~> do
-		need ["lint"]
-		tmp <- hindent srcFile
-		src <- readFile' srcFile
-		if (src == tmp)
-			then putQuiet "No suggestions"
-			else diffHIndent srcFile
+    "checkIndent" ~> do
+        need ["lint"]
+        tmp <- hindent srcFile
+        src <- readFile' srcFile
+        if (src == tmp)
+            then putQuiet "No suggestions"
+            else diffHIndent srcFile
 
 
-	"test" ~> do
-		need [exeFile, "lint", "checkIndent"]
-		cmd exeCmd
+    "test" ~> do
+        need [exeFile, "lint", "checkIndent"]
+        cmd exeCmd
 
-	"submit" ~> do
-		need [srcFile, "test"]
-		cmd "exercism" "submit" srcFile
+    "submit" ~> do
+        need [srcFile, "test"]
+        cmd "exercism" "submit" srcFile
 
-	"fetch" ~> 
-		cmd "exercism" "fetch"
+    "fetch" ~> 
+        cmd "exercism" "fetch"
